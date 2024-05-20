342a6b2d406c74fd6264685dd36786231f35b032
Noor-bot122 <mayaakter7162@gmail.com>
2024-05-20 20:00:48 +0600

    Update adc.js

--- scripts/commands/adc.js
+++ scripts/commands/adc.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "adc",
   version: "1.0.1",
   permission: 2,
-  credits: "D-Jukie",
+  credits: "nazrul",
   description: "Apply code from buildtooldev and pastebin",
   prefix: true,
   category: "admin",
